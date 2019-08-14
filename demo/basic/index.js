5c4e5bab199ddca4269a0b120a5b17374709223f
amazingandyyy <andycandomore@gmail.com>
2019-08-14 14:58:23 -0700

    update style

--- demo/basic/index.js
+++ demo/basic/index.js
@@ -1,6 +1,6 @@
-const nodeServer = require('../../index.js');
-const app = new nodeServer();
-const morgan  = require('morgan')
+const NodeServer = require('../../index.js')
+const app = new NodeServer()
+const morgan = require('morgan')
 
 // app.get('/'. (req, res)=>{
 //    res.send('hello')
@@ -10,12 +10,12 @@ const morgan  = require('morgan')
 // })
 app.use(morgan('dev'))
 
-app.use((req, res, next)=>{
-    next();
+app.use((req, res, next) => {
+  next()
 })
-app.use((req, res, next)=>{
-    res.end('ok')
-    next();
+app.use((req, res, next) => {
+  res.end('ok')
+  next()
 })
 
-app.start({port: '4000'})
+app.start({ port: '4000' })
--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const http = require('http');
+const http = require('http')
 
-class nodeServer {
-  constructor(middlewares=[]) {
-    this.middlewares = [...middlewares];
+class NodeServer {
+  constructor (middlewares = []) {
+    this.middlewares = [...middlewares]
   }
-  start(config) {
+
+  start (config) {
     const handler = (req, res) => {
       this.handle(req, res, (err) => err && res.writeHead(500))
     }
-    return http.createServer(handler).listen(config, ()=>{
-      const port = config.port || config;
+    return http.createServer(handler).listen(config, () => {
+      const port = config.port || config
       console.log(`\x1b[33m[node-server] listening on http://localhost:${port}`, '\x1b[0m')
     })
   }
-  use(middleware) {
-    if (typeof middleware !== 'function') throw new Error('Middleware must be a function!');
-    this.middlewares.push(middleware);
+
+  use (middleware) {
+    if (typeof middleware !== 'function') throw new Error('Middleware must be a function!')
+    this.middlewares.push(middleware)
   }
-  handle(req, res, cb) {
-    let counter = 0;
+
+  handle (req, res, cb) {
+    let counter = 0
     const next = (e) => {
-      if(e!=null) return cb(e);
-      if(counter>=this.middlewares.length) return cb();
-      const middle = this.middlewares[counter++];
+      if (e != null) return cb(e)
+      if (counter >= this.middlewares.length) return cb()
+      const middle = this.middlewares[counter++]
       try {
-        middle(req, res, next);
-      } catch(error) {
-        next(error);
+        middle(req, res, next)
+      } catch (error) {
+        next(error)
       }
     }
-    next();
+    next()
   }
 }
 
-module.exports = nodeServer;
+module.exports = NodeServer
