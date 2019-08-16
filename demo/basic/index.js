b771743fafe3c18c1e1062cafb2f2312729ae0a3
Andy Chen <andycandomore@gmail.com>
2019-08-15 21:40:35 -0700

    update demo

--- demo/basic/index.js
+++ demo/basic/index.js
@@ -3,12 +3,22 @@ const bodyParser = require('@amazingandyyy/body-parser')
 const app = new NodeServer()
 const logger = require('@amazingandyyy/server-logger')
 
-app.on('get', '/', (req, res) => {
+app.on('get', '/', checkUser, checkStatus, (req, res) => {
+  console.log(req.user);
   res.send(200)
 })
 
+function checkUser(req, res, next){
+  req.user={id: '345678987654', status: 'member'};
+  next()
+}
+function checkStatus(req, res, next){
+  if(req.user.status!=='admin') return res.send(401);
+  next()
+}
+
 app.on('post', '/ping', (req, res) => {
-  res.send(req.body)
+  res.send('pong')
 })
 
 app.use(logger('dev'))
--- index.js
+++ index.js
@@ -14,13 +14,21 @@ class NodeServer {
     const handler = (req, res) => {
       this.handle(req, res, (err) => {
         if (err) {
+          console.error(err)
           res.writeHead(500)
-          res.end(err || 'Internal Server Error')
+          return res.end(err || 'Internal Server Error')
         } else if (this.routers[req.method][req.url]) {
-          this.routers[req.method][req.url](req, res)
+          const [middlewares, cb] = this.routers[req.method][req.url];
+          this.iterateMiddlewareAndCb(req, res, middlewares, cb, (err)=>{
+            if (err) {
+              console.error(err)
+              res.writeHead(500)
+              return res.end(err || 'Internal Server Error')
+            }
+          })
         } else {
           res.writeHead(404)
-          res.end('Page Not Found')
+          return res.end('Page Not Found')
         }
       })
     }
@@ -28,6 +36,24 @@ class NodeServer {
       const port = config.port || config
       console.log(`\x1b[33m[node-server] listening on http://localhost:${port}`, '\x1b[0m')
     })
+  }
+
+  iterateMiddlewareAndCb(req, res, middlewares, cb, handleError) {
+    let counter = 0
+    const next = (e) => {
+      if (e != null) return setImmediate(() => handleError(e));
+      if (counter >= middlewares.length) return setImmediate(() => cb(req, res));
+      const middle = middlewares[counter]
+      setImmediate(() => {
+        try {
+          counter++
+          middle(req, res, next)
+        } catch (error) {
+          next(error)
+        }
+      })
+    }
+    next()
   }
 
   use (middleware) {
@@ -39,34 +65,29 @@ class NodeServer {
     res.send = (content) => {
       if (typeof content === 'number') {
         res.writeHead(content)
-        res.end()
+        return res.end()
       }
       if (typeof content === 'string') {
         res.setHeader('Content-Type', 'text/plain')
-        res.end(content)
+        return res.end(content)
       }
       if (typeof content === 'object') {
         res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(content))
+        return res.end(JSON.stringify(content))
       }
     }
     res.html = (content, value = {}) => {
       if (typeof content === 'string') {
         res.writeHead(content)
-        res.end()
+        return res.end()
       } else {
         //  user Logger
       }
     }
     let counter = 0
     const next = (e) => {
-      if (e != null) {
-        console.error(e)
-        return setImmediate(() => cb(e))
-      }
-      if (counter >= this.middlewares.length) {
-        return setImmediate(() => cb())
-      }
+      if (e != null) setImmediate(() => cb(e));
+      if (counter >= this.middlewares.length) return setImmediate(() => cb());
       const middle = this.middlewares[counter]
       setImmediate(() => {
         try {
@@ -80,8 +101,10 @@ class NodeServer {
     next()
   }
 
-  on (method, route, reqres) {
-    this.routers[method.toUpperCase()][route] = reqres
+  on (...args) {
+    const middlewares = args.splice(2, args.length - 3);
+    const [method, route, cb] = args
+    this.routers[method.toUpperCase()][route] = [middlewares, cb]
   }
 }
 
